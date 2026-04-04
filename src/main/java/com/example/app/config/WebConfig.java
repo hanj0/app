2e728395ef4779ed9b992ef3c9dad79da6bf10ff
hanj0 <hanjaeyoung0@gmail.com>
2026-04-04 13:57:34 +0900

    Chore: CORS허용 도메인 수정

--- src/main/java/com/example/app/config/WebConfig.java
+++ src/main/java/com/example/app/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://updown.world", "https://www.updown.world")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
